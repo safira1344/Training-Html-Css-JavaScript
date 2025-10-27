a5311e09018c489da6f7c0dfa64fb975e1a21fd8
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-10-27 16:57:36 -0300

    add: update form structure in HTML and implement event handling in JavaScript

---
+++ jsLogicaProgramacao/calculadoraImc/modelo/assets/js/main.js
@@ -0,0 +1,26 @@
+function main() {
+    const form = document.querySelector('#formulario');
+
+    form.addEventListener('submit', function (e){
+        e.preventDefault();
+        console.log('Evento prevenido');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
