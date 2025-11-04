6631daa640d39b7c3b3cc8fcb85712210ad7aab7
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-03 23:51:08 -0300

    refactor: clean up variable declarations and commented code in index.js

--- jsLogicaProgramacao/aula30/index.js
+++ jsLogicaProgramacao/aula30/index.js
@@ -1,10 +1,41 @@
 const verdadeira = true;
 
-let nome = 'Luiz';
-var nome2 = 'Luiz';
+// Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função
 
-if (verdadeira) {
-    let nome = 'Otávio';
-    console.log(nome, nome2); 
+// let nome = 'Luiz'; //criando
+// var nome2 = 'Luiz';
+
+// if (verdadeira) {
+//     let nome = 'Otávio'; //criando
+//     console.log(nome, nome2); 
+
+//     if (verdadeira) {
+//         let nome = 'Outra coisa'; //criando
+//         console.log(nome, nome2);
+//     }
+// }
+
+
+// let nome = 'Luiz'; 
+// var nome2 = 'Luiz';
+
+// if (verdadeira) {
+//     let nome = 'Otávio'; 
+//     var nome2 = 'Rogério'; //redeclarando
+
+//     if (verdadeira) {
+//         var nome2 = 'Ronaldo' //redeclarando
+//         let nome = 'Outra coisa'; 
+//     }
+// }
+
+// console.log(nome, nome2);
+
+
+function falaOi () {
+    let nome = 'Fernanda';
 }
 
+console.log (nome);
+falaOi();
