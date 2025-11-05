199d8711c2bbabc998451c7e8cb9cea78b59f8d2
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-04 22:23:56 -0300

    add: extend array destructuring examples in index.js

--- jsLogicaProgramacao/aula31/index.js
+++ jsLogicaProgramacao/aula31/index.js
@@ -21,3 +21,11 @@ console.log(resto);
 
 const [um, , tres, , cinco, , sete] = numeros;
 console.log(um, tres, cinco, sete);
+
+
+// --------------------------------------------------------------
+//                   0         1       2
+//                 0 1 2    0 1 2    0 1 2
+const numerosArray = [ [1,2,3], [4,5,6], [7,8,9] ];
+const [lista1, lista2, lista3] = numerosArray; //desestruturação de array
+console.log(lista3[2]);
