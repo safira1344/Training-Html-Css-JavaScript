5ced41d1f9536f60d08609b94e1685b5746e0141
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-04 22:20:10 -0300

    refactor: update variable assignment and logging with array destructuring in index.js

--- jsLogicaProgramacao/aula31/index.js
+++ jsLogicaProgramacao/aula31/index.js
@@ -2,7 +2,22 @@ let a = 'A'; //B
 let b = 'B'; //C
 let c = 'C'; //A
 
-const numeros = [1, 2, 3];
-[a, b, c] = numeros;
+// const numeros = [1, 2, 3];
+// [a, b, c] = numeros;
+// console.log(a, b, c);
 
-console.log(a, b, c);
+const letras = [b,c,a]; //desestruturação de array
+[a, b, c] = letras;
+console.log(a,b,c);
+
+
+// -----------------------------------------------------------
+
+// ... rest     ... spread
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [primeiroNumero, segundoNumero, ... resto] = numeros;
+console.log(primeiroNumero, segundoNumero);
+console.log(resto);
+
+const [um, , tres, , cinco, , sete] = numeros;
+console.log(um, tres, cinco, sete);
