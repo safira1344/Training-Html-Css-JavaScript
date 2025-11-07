b7a20f09bf5aaa1e17b461e724219f93f80c5b05
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-06 22:58:27 -0300

    refactor: clean up desestruturacao.js by removing commented code and fixing indentation

--- jsLogicaProgramacao/aula31/desestruturacao.js
+++ jsLogicaProgramacao/aula31/desestruturacao.js
@@ -2,14 +2,56 @@ const pessoa = {
     nome: 'Fernanda',
     sobrenome: 'Mirely',
     idade: 20,
-    endereço: {
+    endereco: {
         rua: 'Av Brasil',
-        numero: 97,
+        numero: 97
     }
 };
 
 // const nome = pessoa.nome;
 // console.log(nome);
 
-const {nome} = pessoa;
-console.log(nome);
+
+//atribuiçao via desestruturação
+// const {nome, sobrenome, idade} = pessoa;
+// console.log(nome, sobrenome, idade);
+
+
+// const { nome = 'Não existe', sobrenome } = pessoa;
+// console.log(nome, sobrenome);
+
+
+//atribuição de varíavel por meio da desestruturação
+// const { nome: teste = '', sobrenome } = pessoa;
+// console.log(teste, sobrenome);
+
+
+// const { 
+//     endereco: {rua, numero}, 
+//     endereco
+// } = pessoa;
+// console.log(rua, numero, endereco);
+
+
+//atrubuição de variável por desestruturação
+// const {
+//     endereco: { rua: r = 12345, numero },
+//     endereco
+// } = pessoa;
+// console.log(r, numero, endereco);
+
+
+// ... rest  ...spread
+// const { nome, ...resto } = pessoa;
+// console.log(resto);
+
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 == 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(i);
+}
