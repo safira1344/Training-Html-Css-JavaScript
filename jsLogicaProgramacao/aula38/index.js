ab83070d6b61e6b3a4b278656ab3d3b5d7b2101b
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-18 23:53:35 -0300

    refactor: enhance for-in loop logic in index.js with conditional statements

--- jsLogicaProgramacao/aula38/index.js
+++ jsLogicaProgramacao/aula38/index.js
@@ -1,5 +1,17 @@
 const numeros = [1,2,3,4,5,6,7,8,9];
 
 for (let numero in numeros) {
+    
+    if(numero === 2) {
+        console.log('Pulei o número 2');
+        continue;
+    }
+
     console.log(numero);
+
+    if(numero === 7) {
+        console.log('7 encontrado, saindo...');
+        break;
+    }
+
 }
