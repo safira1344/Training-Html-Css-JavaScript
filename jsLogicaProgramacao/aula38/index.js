fb66209d995270f4b84b08e13179f98f106f6035
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-18 23:56:35 -0300

    refactor: comment out for-in loop example in index.js and retain while loop logic

--- jsLogicaProgramacao/aula38/index.js
+++ jsLogicaProgramacao/aula38/index.js
@@ -1,9 +1,28 @@
 const numeros = [1,2,3,4,5,6,7,8,9];
 
-for (let numero in numeros) {
+// for (let numero in numeros) {
     
+//     if(numero === 2) {
+//         console.log('Pulei o número 2');
+//         continue;
+//     }
+
+//     console.log(numero);
+
+//     if(numero === 7) {
+//         console.log('7 encontrado, saindo...');
+//         break;
+//     }
+
+// }
+
+let i = 0;
+while (i < numeros.length) {
+    let numero = numeros[i];
+
     if(numero === 2) {
         console.log('Pulei o número 2');
+        i++
         continue;
     }
 
@@ -14,4 +33,5 @@ for (let numero in numeros) {
         break;
     }
 
+    i++;
 }
