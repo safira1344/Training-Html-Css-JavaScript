1a2fd7e814e353c6aa8bc934402bf97b32878f21
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-20 15:00:32 -0300

    refactor: streamline maiorNumero function implementation in index.js

--- jsLogicaProgramacao/aula39/index.js
+++ jsLogicaProgramacao/aula39/index.js
@@ -6,15 +6,20 @@
 
 // console.log(maiorNumero(2,4));
 
-// function maiorNumero (num1, num2) {
-//     const numeros = [num1, num2];
-//     if(num1 > num2) return num1;
-//     else return num2;
-// }
-
 function maiorNumero (num1, num2) {
-    const numeros = [num1, num2];
-    return (num1 > num2)? num1 : num2;
+    if(num1 > num2) return num1;
+    return num2;
 }
 
-console.log(maiorNumero(10,30));
+// function maiorNumero (num1, num2) {
+//     return (num1 > num2)? num1 : num2;
+// }
+
+
+//arrow function
+const max2 = (x,y) => {
+    return x>y ? x:y;
+};
+
+console.log(maiorNumero(50,30));
+console.log(max2(50,30));
