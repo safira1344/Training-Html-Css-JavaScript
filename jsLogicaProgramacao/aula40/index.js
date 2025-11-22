b9153dbd6a1f7e15400bd496545334a458913844
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-21 21:50:48 -0300

    refactor: add ePaisagem function implementation in index.js

---
+++ jsLogicaProgramacao/aula40/index.js
@@ -0,0 +1,9 @@
+// function ePaisagem (largura, altura) {
+//     return largura >= altura;
+// }
+
+const ePaisagem = (largura, altura) => {
+    return largura >= altura;
+};
+
+console.log(ePaisagem(70, 80));
