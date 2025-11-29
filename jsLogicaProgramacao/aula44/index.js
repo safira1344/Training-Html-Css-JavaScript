ee29f55a6f954d204bff5d70fbb833b906024475
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-11-28 21:23:11 -0300

    refactor: comment out unused timer and timeout functions in index.js

--- jsLogicaProgramacao/aula44/index.js
+++ jsLogicaProgramacao/aula44/index.js
@@ -6,16 +6,24 @@ function mostraHora() {
     });
 }
 
+//setInterval agenda a execução de uma função repetidamente, com um intervalo fixo entre invocações (também em ms).
+//setTimeout agenda a execução de uma função uma única vez depois de um atraso (em milissegundos).
+
 
 //é possível criar funções anônimas "function()" dentro de argumento de chamada de função
-const timer = setInterval(function() {
-    console.log(mostraHora());
-}, 10000); //esse 10000 é 10 seg
+// const timer = setInterval(function() {
+//     console.log(mostraHora());
+// }, 10000); //esse 10000 é 10 seg
 
-setTimeout(function() {
-    clearInterval(timer);
-}, 3000);
+// setTimeout(function() {
+//     clearInterval(timer);
+// }, 3000);
 
-setTimeout(function() {
-    console.log('Olá mundo!');
-}, 5000);
+// setTimeout(function() {
+//     console.log('Olá mundo!');
+// }, 5000);
+
+function saudacao(nome) {
+    console.log('Olá,', nome);
+}
+setTimeout(saudacao, 5000, 'Fernanda'); // "Olá, Fernanda" após 5s
