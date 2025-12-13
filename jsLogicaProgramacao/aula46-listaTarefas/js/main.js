6c16632aad20146185b114a63774527ef82ba4a7
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2025-12-12 23:56:15 -0300

    add: funcionalidade de criar uma tarefa

--- jsLogicaProgramacao/aula46-listaTarefas/js/main.js
+++ jsLogicaProgramacao/aula46-listaTarefas/js/main.js
@@ -2,7 +2,11 @@ const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.btn-tarefa');
 
+function criaTarefa(textoInput) {
+    console.log(textoInput);
+}
 
 btnTarefa.addEventListener('click', function(e) {
-    console.log(inputTarefa.value);
+    if(!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
 });
