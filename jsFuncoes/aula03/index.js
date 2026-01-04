c6409b7573eb420adb34533050c29940345584f4
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-01-04 00:13:47 -0300

    add: implementa funções para criar pessoas e multiplicadores

--- jsFuncoes/aula03/index.js
+++ jsFuncoes/aula03/index.js
@@ -2,8 +2,43 @@
 //Retorna um valor
 //Termina a função
 
-function soma(a, b) {
-    return a + b;
+function criaPessoa(nome,sobrenome) {
+    return {nome: nome, sobrenome: sobrenome};
 }
 
-soma(2,3);
+const p1 = criaPessoa('fernanda', 'mirely');
+const p2 = {
+    nome: 'nathalia',
+    sobrenome: 'barbosa'
+};
+console.log(p1);
+console.log(p2);
+
+
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+const olaMundo = falaFrase('Olá');
+console.log(olaMundo);
+console.log(olaMundo('Mundo.'));
+
+
+
+function criaMultiplicador(multiplicador) {
+    // multiplicador
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadruplica = criaMultiplicador(4);
+
+
+console.log(duplica(3));
+console.log(triplica(4));
+console.log(quadruplica(10));
