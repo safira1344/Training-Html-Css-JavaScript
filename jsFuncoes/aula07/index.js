f2e2009663a3ba191e946d31644f9ec8b8284b72
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-01-12 21:59:23 -0300

    fix: correct 'fala' method to use 'this' context and add 'altura' and 'peso' parameters to 'criaPessoa'

--- jsFuncoes/aula07/index.js
+++ jsFuncoes/aula07/index.js
@@ -3,10 +3,17 @@ function criaPessoa (nome, sobrenome) {
         nome,
         sobrenome,
         fala: function (assunto) {
-            return `${nome} está ${assunto}.`;
+            return `${this.nome} está ${assunto}.`;
         },
+        altura: a,
+        peso: p,
+        imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio');
+const p1 = criaPessoa('Fernanda', 'Mirely', 1.8, 80);
+console.log(p1.imc());
 console.log(p1.fala('falando sobre JS'));
