ef535dba08604ba18c8f33ed3398f5416fee01b8
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-01-31 23:46:09 -0300

    add: fix reduce example to correctly sum numbers and log the total

--- jsFuncoes/aula18/atividade-reduce.js
+++ jsFuncoes/aula18/atividade-reduce.js
@@ -1,8 +1,13 @@
-//Some todos os números (reduce)
 //Retorne um array com os pares (Filter)
 //Retorne um array com o dobro dos valores (Map)
 
+
+
+
+//Some todos os números (reduce)
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const total = numeros.reduce(function(acumulador, valor, indice, array) {
-    console.log(acumulador);
+    acumulador += valor;
+    return acumulador;
 }, 0);
+console.log(total);
