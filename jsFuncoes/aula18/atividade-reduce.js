2a1666b06dbbf8f6298a60289493d9db114f1692
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-02-01 00:11:06 -0300

    refactor: update comments and structure for reduce examples

--- jsFuncoes/aula18/atividade-reduce.js
+++ jsFuncoes/aula18/atividade-reduce.js
@@ -1,13 +1,41 @@
 //Retorne um array com os pares (Filter)
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const total = numeros.reduce(function(acumulador, valor, indice, array) {
+//     if (valor % 2 === 0) acumulador.push(valor);
+//     return acumulador;
+// }, []);
+// console.log(total);
+
+
 //Retorne um array com o dobro dos valores (Map)
-
-
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const total = numeros.reduce(function(acumulador, valor, indice, array) {
+//     acumulador.push(valor * 2);
+//     return acumulador;
+// }, []);
+// console.log(total);
 
 
 //Some todos os números (reduce)
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce(function(acumulador, valor, indice, array) {
-    acumulador += valor;
-    return acumulador;
-}, 0);
-console.log(total);
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const total = numeros.reduce(function(acumulador, valor, indice, array) {
+//     acumulador += valor;
+//     return acumulador;
+// }, 0);
+// console.log(total);
+
+
+//Retorne a pessoa mais velha
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 105 },
+    { nome: 'Letícia', idade: 10 },
+    { nome: 'Rosana', idade: 64 },
+    { nome: 'Wallace', idade: 1000 },
+];
+const maisVelha = pessoas.reduce(function(acumulador, valor){
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+console.log(maisVelha);
