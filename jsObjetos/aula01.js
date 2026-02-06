a694913226cc062df7565be28943cc579308113a
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-02-05 22:44:49 -0300

    refactor: comment out previous pessoa1 implementation and clean up code structure

--- jsObjetos/aula01.js
+++ jsObjetos/aula01.js
@@ -19,16 +19,45 @@
 // console.log(pessoa1, pessoa2);
 
 
-const pessoa1 = new Object();
-pessoa1.nome = 'Luiz';
-pessoa1.sobrenome = 'Otávio';
-pessoa1.idade = 21;
-pessoa1.falarNome = function() {
-    return (`${this.nome} está falando seu nome.`);
-};
-pessoa1.getDataNascimento = function() {
-    const dataAtual = new Date();
-    return dataAtual.getFullYear() - this.idade;
-};
+// const pessoa1 = new Object();
+// pessoa1.nome = 'Luiz';
+// pessoa1.sobrenome = 'Otávio';
+// pessoa1.idade = 21;
+// pessoa1.falarNome = function() {
+//     return (`${this.nome} está falando seu nome.`);
+// };
+// pessoa1.getDataNascimento = function() {
+//     const dataAtual = new Date();
+//     return dataAtual.getFullYear() - this.idade;
+// };
 
-console.log(pessoa1.getDataNascimento());
+// console.log(pessoa1.getDataNascimento());
+
+
+//Factory functions 
+// function criaPessoa(nome, sobrenome) {
+//     return {
+//         nome,
+//         sobrenome,
+//         nomeCompleto() {
+//             return `${this.nome} ${this.sobrenome}`;
+//         }
+//     };
+// }     
+
+// const p1 = criaPessoa('Luiz', 'Otávio');
+// console.log(p1.nomeCompleto());
+
+// Constructor functions
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+// {} <- this -> this
+const p1 = new Pessoa('Fernanda', 'Souza');
+console.log(p1);
+
+const p2 = {};
+p2.nome = 'Luiz';
+console.log(p2);
