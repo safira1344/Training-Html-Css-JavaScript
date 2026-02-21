a569eb0bf3ac61dd45c49f2baeccd6f352c5c090
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-02-21 00:20:38 -0300

    add: create index.js with initial object manipulation examples

---
+++ jsObjetos/aula04/index.js
@@ -0,0 +1,20 @@
+/*
+Object.values
+Object.entries
+Object.assign(des, any)
+Object.getOwnPropertyDescriptor(o, 'prop')
+... (spread)
+
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
+
+const produto = {nome: 'Produto', preco: 1.8};
+const caneca = Object.assign({}, produto, {material: 'porcelana'});
+
+caneca.nome = 'Outro nome';
+caneca.preco = 2.5;
+console.log(produto);
+console.log(caneca);
