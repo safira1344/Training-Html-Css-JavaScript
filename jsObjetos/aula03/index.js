83483d432f4f3501cfeafdf14baffdaf5b546d49
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-02-21 00:03:21 -0300

    refactor: clean up Produto constructor and add criaProduto function

--- jsObjetos/aula03/index.js
+++ jsObjetos/aula03/index.js
@@ -1,4 +1,6 @@
 // defineProperty -> Getter e Setters
+
+// constructor function
 function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
@@ -14,6 +16,28 @@ function Produto(nome, preco, estoque) {
             if (typeof valor !== 'number') {
                 throw new TypeError('Mensagem');
             }
+
+            estoquePrivado = valor;
         }
-    })
+    });
 }
+
+// fabric function
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome;
+        },
+        set nome(valor) {
+            nome = valor;
+        }
+    };
+}
+
+// const p1 = new Produto ('Camiseta', 20, 3);
+// console.log(p1);
+// p1.estoque = 'asdasd';
+// console.log(p1.estoque);
+
+const p2 = criaProduto('Camiseta');
+console.log(p2.nome);
