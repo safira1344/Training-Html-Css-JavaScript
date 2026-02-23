594bf06c0f725260e9dcaef8b21a27e71dcdfbb5
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-02-22 23:49:12 -0300

    refactor: comment out previous examples and update produto definition

--- jsObjetos/aula04/index.js
+++ jsObjetos/aula04/index.js
@@ -9,12 +9,34 @@ Object.keys (retorna as chaves)
 Object.freeze (congela o objeto)
 Object.defineProperties (define várias propriedades)
 Object.defineProperty (define uma propriedade)
+--------------------------------------------------------------
 */
 
-const produto = {nome: 'Produto', preco: 1.8};
-const caneca = Object.assign({}, produto, {material: 'porcelana'});
+// const produto = {nome: 'Produto', preco: 1.8};
+// const caneca = Object.assign({}, produto, {material: 'porcelana'});
 
-caneca.nome = 'Outro nome';
-caneca.preco = 2.5;
-console.log(produto);
-console.log(caneca);
+// const produto = {nome: 'Produto', preco: 1.8};
+// Object.freeze(produto);
+// produto.nome = 'Outro nome';
+// const caneca = {nome: produto.nome, preco: produto.preco};
+// console.log(Object.keys(produto));
+
+// caneca.nome = 'Outro nome';
+// caneca.preco = 2.5;
+// console.log(produto);
+// console.log(caneca);
+
+// const produto = {nome: 'Produto', preco: 1.8};
+// Object.defineProperty(produto, 'nome', {
+//     writable: false,
+//     configurable: false,
+//     value: 'Qualquer outra coisa'
+// });
+// console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+// console.log(produto);
+
+const produto = { nome: 'Produto', preco: 1.8, material: 'porcelana'};
+
+for (let valor of Object.entries(produto)) {
+    console.log(valor[0], valor[1]);
+} 
