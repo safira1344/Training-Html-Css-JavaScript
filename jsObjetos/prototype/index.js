caa07c6577ee695b9ad079e2f2f9bde56abc311d
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-03-04 21:06:59 -0300

    add: implement Produto constructor and methods for object manipulation

--- jsObjetos/prototype/index.js
+++ jsObjetos/prototype/index.js
@@ -1,8 +1,18 @@
+//JavaScript usa herança prototipal
+
+// Procura a propriedade no próprio objeto.
+// Se não encontrar, procura no protótipo.
+// Continua subindo na cadeia até chegar em null.
+
 // new Object -> Object.prototype
 const objA = {
     chaveA: 'A'
     //__proto__: Object.prototype
 };
+
+// __proto__ é legado
+// Não é recomendado oficialmente
+// Pode ser removido no futuro
 
 const objB = {
     chaveB: 'B'
@@ -15,3 +25,6 @@ objC.chaveC = 'C';
 Object.setPrototypeOf(objB, objA);
 Object.setPrototypeOf(objC, objB);
 console.log(objC.chaveA);
+
+// Em vez de usar setPrototypeOf, prefira:
+// Object.create() (assim o protótipo já é definido na criação - muito mais eficiente).
