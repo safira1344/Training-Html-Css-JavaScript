89635be6a1017fafb5aa60693de8aace5f60815f
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-03-05 23:53:56 -0300

    add: implement Produto class with aumento and desconto methods

---
+++ jsObjetos/heranca/index.js
@@ -0,0 +1,16 @@
+// Produto -> aumento, desconto
+// Camiseta = Cor, caneca = material
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+}
