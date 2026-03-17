339ecfbb57389f3dc5774b7f6ecd95abc7eead13
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-03-16 21:57:45 -0300

    refactor: simplify criaPessoa function by using prototype delegation for methods

--- jsObjetos/factory_prototype/index.js
+++ jsObjetos/factory_prototype/index.js
@@ -1,21 +1,73 @@
 //Factory function
+// function criaPessoa(nome, sobrenome) {
+//     return {
+//         nome,
+//         sobrenome,
+
+//         falar() {
+//             console.log(`${this.nome} está falando.`);
+//         },
+
+//         comer() {
+//             console.log(`${this.nome} está comendo.`);
+//         },
+
+//         beber() {
+//             console.log(`${this.nome} está bebendo.`);
+//         },
+//     };
+// }
+
+//ou
+// function criaPessoa(nome, sobrenome) {
+//     const pessoaPrototype = {
+//         falar() {
+//             console.log(`${this.nome} está falando.`);
+//         },
+
+//         comer() {
+//             console.log(`${this.nome} está comendo.`);
+//         },
+
+//         beber() {
+//             console.log(`${this.nome} está bebendo.`);
+//         },
+//     };
+
+//     return Object.create(pessoaPrototype, {
+//         nome: {value: nome},
+//         sobrenome: {value: sobrenome}
+//     });
+// }
+
+//ou
+
+const falar = {
+    falar() {
+        console.log(`${this.nome} está falando.`);
+    },
+};
+
+const comer = {
+    comer() {
+        console.log(`${this.nome} está comendo.`);
+    },
+};
+
+const beber = {
+    beber() {
+        console.log(`${this.nome} está bebendo.`);
+    },
+};
+
+// const pessoaPrototype = Object.assign({}, falar, comer, beber);
+const pessoaPrototype = {...falar, ...beber, ...comer};
+
 function criaPessoa(nome, sobrenome) {
-    return {
-        nome,
-        sobrenome,
-
-        falar() {
-            console.log(`${this.nome} está falando.`);
-        },
-
-        comer() {
-            console.log(`${this.nome} está comendo.`);
-        },
-
-        beber() {
-            console.log(`${this.nome} está bebendo.`);
-        },
-    };
+    return Object.create(pessoaPrototype, {
+        nome: {value: nome},
+        sobrenome: {value: sobrenome}
+    });
 }
 
 const p1 = criaPessoa('Fernanda', 'Mirely');
