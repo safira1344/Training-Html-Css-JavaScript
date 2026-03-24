dc13c19d340b88537f9b03b06722319da7bba68e
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-03-24 17:37:05 -0300

    add: implement ControleRemoto class with methods to adjust volume

---
+++ jsObjetos/instancia_estatica/index.js
@@ -0,0 +1,25 @@
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    aumentarVolume() {
+        this.volume += 2;
+    }
+
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    trocaPilha() {
+
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.diminuirVolume();
+console.log(controle1);
