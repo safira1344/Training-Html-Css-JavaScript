757a9a2cfa4aeae7b1c1d56cdc51d0160c7b3d8e
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-03-24 23:30:43 -0300

    refactor: add comments to methods and adjust formatting in ControleRemoto class

--- jsObjetos/instancia_estatica/index.js
+++ jsObjetos/instancia_estatica/index.js
@@ -4,6 +4,7 @@ class ControleRemoto {
         this.volume = 0;
     }
 
+    //método de instância
     aumentarVolume() {
         this.volume += 2;
     }
@@ -12,14 +13,17 @@ class ControleRemoto {
         this.volume -= 2;
     }
 
-    trocaPilha() {
-
+    //método estático
+    static soma(x, y) {
+        console.log(this);
     }
 }
 
 const controle1 = new ControleRemoto('LG');
-controle1.aumentarVolume();
+controle1.aumentarVolume(); //precisa da instância criada para usar os métodos
 controle1.aumentarVolume();
 controle1.aumentarVolume();
 controle1.diminuirVolume();
 console.log(controle1);
+
+ControleRemoto.soma(); //utiliza a classe pra chamar o método
