bd34c5cb12c6691ed6feb7695b769073ea117506
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-04-26 23:41:35 -0300

    update: comment out unused code and restructure Pessoa class in mod1.js and app.js

--- node/aula1/app.js
+++ node/aula1/app.js
@@ -1,2 +1,13 @@
-const mod1 = require('./mod1');
-console.log(mod1);
+// const mod1 = require('./mod1');
+// console.log(mod1.falaNome());
+
+// const {nome, sobrenome, falaNome} = require('./mod1');
+// console.log(nome,sobrenome);
+// console.log(falaNome());
+
+const path = require('path');
+// const axios = require('axios');
+const { Pessoa } = require('./mod1');
+
+const p1 = new Pessoa('Fernanda');
+console.log(p1);
--- node/aula1/mod1.js
+++ node/aula1/mod1.js
@@ -1,16 +1,28 @@
-const nome = 'Fernanda'
-const sobrenome = 'Mirely';
+// const nome = 'Fernanda'
+// const sobrenome = 'Mirely';
 
-const falaNome = () => {
-    console.log(nome,sobrenome);
-};
+// const falaNome = () => nome + ' ' + sobrenome;
 
 
 // module.exports.nome = nome;
 // module.exports.sobrenome = sobrenome;
 // module.exports.falaNome = falaNome;
+// console.log(module.exports);
 
-exports.nome = nome;
-exports.sobrenome = sobrenome;
-exports.falaNome = falaNome;
-this.qualquerCoisa = 'O que eu quiser exportar';
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'O que eu quiser exportar';
+// console.log(exports);
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+        this.sobrenome = this.sobrenome;
+    }
+}
+
+const nome = 'Fernanda';
+const sobrenome = 'Mirely';
+
+exports.Pessoa = Pessoa;
