6d97014dfd79aae36f04186dbe2057ee716abe64
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-05-08 17:45:56 -0300

    add: update package.json with description and devDependencies; enhance server.js with additional comments and structure

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -1,3 +1,31 @@
-//criar arquivos com node
 const express = require('express');
 const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//          POST   GET    PUT    DELETE
+
+//http://meusite.com/ <- GET -> Entregue a página
+//http://meusite.com/sobre <- GET -> Entregue a página
+//http://meusite.com/contato <- GET -> Entregue a página
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </form>
+        `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário!');
+})
+
+app.get('/contato', (req,res) => {
+    res.send('Obrigado por entrar em contato com a gente.');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
