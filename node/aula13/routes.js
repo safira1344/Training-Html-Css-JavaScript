9d4f5fcb833c046e099a16c595ce3b3ab278994f
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-05-20 21:35:50 -0300

    feat: update project name to aula13 and add form for client input in index.ejs

--- node/aula13/routes.js
+++ node/aula13/routes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
+const meuMidleware = require('./src/middlewares/middleware');
+
+// function meuMidleware(req, res, next) {
+//     req.session = {nome:'Fernanda', sobrenome: Mirely};
+//     console.log();
+//     console.log('Passei no seu middleware.');
+//     console.log();
+//     next();
+// } 
 
 //rotas da home
 route.get('/', homeController.paginaInicial);
