a39528d2753dedfacd44cdd6cc38e1b547f8ed86
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-05-25 23:03:04 -0300

    feat: update MongoDB connection string and restructure server initialization

--- node/aula14/server.js
+++ node/aula14/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://safiraaabarbosaaa_db_user:<db_password>@cursojs01.vvnaehq.mongodb.net/?appName=cursojs01';
-mongoose.connect();
+const connectionString = 'mongodb+srv://safiraaabarbosaaa_db_user:<db_password>@cursojs01.vvnaehq.mongodb.net/BASEDEDADOS?appName=cursojs01';
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.emit('pronto');
+    });
 
 const routes = require('./routes');
 const path = require('path');
-const {middlewareGlobal, outroMiddleware} = require('./src/middlewares/middleware');
+const {middlewareGlobal} = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({extended: true}));
 
@@ -18,10 +20,11 @@ app.set('view engine', 'ejs');
 
 //Nossos próprios middlewares
 app.use(middlewareGlobal);
-app.use(outroMiddleware);
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
 });
