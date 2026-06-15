15825a419cc44000dac8a8c647d742464232a03b
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-06-15 20:28:19 -0300

    style: format session options and server listen callback for consistency

--- projeto-agenda/server.js
+++ projeto-agenda/server.js
@@ -22,14 +22,14 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
+    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
 });
 
 app.use(sessionOptions);
@@ -46,8 +46,8 @@ app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
-  app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
-  });
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
 });
