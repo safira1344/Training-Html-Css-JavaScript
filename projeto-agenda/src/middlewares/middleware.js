d73370e300f0a81da3dfdc7c92168adb621c417e
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-06-24 00:05:25 -0300

    fix: format middleware functions for consistent styling

--- projeto-agenda/src/middlewares/middleware.js
+++ projeto-agenda/src/middlewares/middleware.js
@@ -3,18 +3,18 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.user = req.session.user;
     next();
-    };
+};
 
-    exports.outroMiddleware = (req, res, next) => {
-    next();
-    };
+exports.outroMiddleware = (req, res, next) => {
+next();
+};
 
-    exports.checkCsrfError = (err, req, res, next) => {
+exports.checkCsrfError = (err, req, res, next) => {
     if(err) {
         return res.render('404');
     }
 
-    next();
+next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
