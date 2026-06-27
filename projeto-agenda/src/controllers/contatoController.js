542c69e03f55562d3015e2302a20c0b13a3ef660
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-06-27 00:14:08 -0300

    fix: update redirect logic in delete method to use referrer or default to home

--- projeto-agenda/src/controllers/contatoController.js
+++ projeto-agenda/src/controllers/contatoController.js
@@ -63,6 +63,6 @@ exports.delete = async function(req, res) {
     if(!contato) return res.render('404');
 
     req.flash('success', 'Contato apagado com sucesso.');
-    req.session.save(() => res.redirect('back'));
+    req.session.save(() => res.redirect(req.get('Referrer') || '/'));
     return;
 };
