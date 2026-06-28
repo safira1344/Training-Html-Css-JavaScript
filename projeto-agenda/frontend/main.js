469fc2844596a14a0dd89b70109b191e44d7d71a
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-06-28 16:43:44 -0300

    feat: initialize login functionality with separate instances for login and registration forms

--- projeto-agenda/frontend/main.js
+++ projeto-agenda/frontend/main.js
@@ -1,1 +1,9 @@
-import './assets/css/style.css';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import Login from './modules/Login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
